cd6f151c83935e514ae829f99c063c005fd6d8dc
aaroncaba <aaroncaba@yahoo.com>
2021-01-02 16:43:45 -0500

    Change POST to GET, form for simulating data input

--- addReading.php
+++ addReading.php
@@ -1,4 +1,7 @@
 <?php
+// Use as:  
+// http://hvac-monitor/addReading.php?signal_id=9&state=off
+
 $hostname = "hvac-monitor";
 $username = "arduino";
 $password = "nml0vOeQW3WmzxqY";
@@ -10,17 +13,16 @@ if ($dbconnect->connect_error) {
   die("Database connection failed: " . $dbconnect->connect_error);
 }
 
-if(isset($_POST['submit'])) {
-  $signal_id=$_POST['signal_id'];
-  $state=$_POST['state'];
-  
-  $query = "INSERT INTO states (signal_id, state)
-  VALUES ('$signal_id', '$state')";
-  
-  if (!mysqli_query($dbconnect, $query)) {
-        die('An error occurred when submitting your review.');
-    } else {
-      echo "Thanks for your review.";
-    }	
-}
+$signal_id=$_GET['signal_id'];
+$state=$_GET['state'];
+
+$query = "INSERT INTO states (signal_id, state)
+VALUES ('$signal_id', '$state')";
+
+if (!mysqli_query($dbconnect, $query)) {
+	die('An error occurred when submitting your review.');
+} else {
+	echo "Thanks for your review.";
+}	
+
 ?>
