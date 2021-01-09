8c8f2d1c0d1d396b7f235b0b1d2bb14a13ed5eb5
Aaron Caba <aaroncaba@yahoo.com>
2021-01-09 17:25:09 -0500

    Talking to display

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,18 @@
-
-/*
-  Web client
-
- This sketch connects to a website (http://www.google.com)
- using the WiFi module.
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2)
-
- created 13 July 2010
- by dlf (Metodo2 srl)
- modified 31 May 2012
- by Tom Igoe
- */
-
-#include <SPI.h>
 #include <WiFiNINA.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #include "wifiConnect.h"
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+#define SSD1306_ADDR 0x3C // SSD1306 display I2C address
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
@@ -38,12 +30,30 @@ void setup()
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
+  Serial.println("Starting up...");
+
+  // Initialize the display
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SSD1306_ADDR))
+  { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ; // Don't proceed, loop forever
+  }
+  display.display();
+  display.clearDisplay();
+  display.drawPixel(10,10, SSD1306_WHITE);
+  display.display();
+
   if (connectToWifi() != 0)
   {
     Serial.println("Failed to connect to WiFi");
     while (true) // Pause forever
       ;
   };
+  Serial.println("WiFi Connected");
+
+  Wire.begin();
 
   Serial.println("\nStarting connection to server...");
   // if you get a connection, report back via serial:
